5a6fea6a1314759d9dd510f691911c8e9f655f09
zahid072 <jahidhasan19368@gmail.com>
2024-03-10 21:19:02 +0600

    add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ useEffect(() => {
 
   return (
     <>
+    <div className="w-full h-32 pt-8 bg-slate-200">
+      <div className=' w-96 bg-white rounded-xl p-2 mx-auto flex'>
+        <input className=' bg-transparent w-full outline-none px-5' type="text" />
+        <button className='btn btn-primary'>Search</button>
+      </div>
+    </div>
      <div className='flex gap-5 flex-wrap'>
       
      {
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -6,11 +6,13 @@ const Country = ({country}) => {
     <div>
  <div className="card w-96 bg-base-100 shadow-xl">
   <figure><img src={country.flags.png} className=' h-64' alt="Shoes" /></figure>
-  <div className="pt-5 text-start">
+  <div className="card-body pl-0 pt-5 text-start">
     <h2 className="card-title">Name: {country.name.common}</h2>
     <p>Capital: {country.capital}</p>
-    
   </div>
+  <div className="card-actions">
+      <button className="btn btn-primary">See All Details</button>
+    </div>
   </div>
     </div>
   )
